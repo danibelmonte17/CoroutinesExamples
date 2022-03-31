4636f5b9271183f35e5405bc1c468666c67a94ce
Dani <danib.developer@gmail.com>
2022-03-31 21:42:06 +0200

    Lambda

--- FundamentosCoroutinas/app/src/main/java/com/example/fundamentoscoroutinas/PreviousBases.kt
+++ FundamentosCoroutinas/app/src/main/java/com/example/fundamentoscoroutinas/PreviousBases.kt
@@ -2,6 +2,22 @@ package com.example.fundamentoscoroutinas
 
 fun main(){
 
-    println("Hola Mundo!")
+    lambda()
 
+    multiLambda(3, 3) { result ->
+        println(result)
+    }
+
+}
+
+fun lambda(){
+    println(multi(2, 3))
+}
+
+fun multiLambda(x: Int, y:Int, callback: (result: Int) -> Unit){
+    callback(x*y)
+}
+
+fun multi(x: Int, y:Int): Int{
+    return x*y
 }
